6f3438d7094f7ff77bb8ae8cfa142d69be67efd1
muradtag <murad.tag.97@gmail.com>
2022-03-18 20:59:20 +0200

    apollo client and components setup

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+	return (
+		<header>
+			<div className="categories">{}</div>
+			<div className="logo"></div>
+			<div className="options"></div>
+		</header>
+	);
+}
+
+export default Header;
